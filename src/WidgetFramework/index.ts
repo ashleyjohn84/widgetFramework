e3ac831a7ebb5ab334898b105fa5f9d74f23200f
Anoop Muraleedharan <anmura@microsoft.com>
2018-04-09 14:09:15 +0530

    Refac-removed widgetframework folder

--- src/WidgetFramework/index.ts
+++
@@ -1,5 +0,0 @@
-export { Loader } from './Loader';
-export { MessagePipelineV2 } from './MessagePipelineV2';
-export { Widget } from './Widget';
-export { WidgetHost } from './WidgetHost';
-export { WidgetRuntime } from './WidgetRuntime';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { WidgetHost }  from './WidgetHost';
-export { Widget } from './Widget';
+export { Loader } from './Loader';
 export { MessagePipelineV2 } from './MessagePipelineV2';
+export { Widget } from './Widget';
+export { WidgetHost } from './WidgetHost';
+export { WidgetRuntime } from './WidgetRuntime';
